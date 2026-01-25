fa1594d84433c843ead489560c5f2ccbdc628811
anas <anas33ezz@gmail.com>
2026-01-26 00:42:58 +0200

    start the project

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:my_project/medica.dart';
+
+void main() {
+  runApp(const Medica());
+}
---
+++ lib/medica.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:my_project/screens/home.dart';
+
+class Medica extends StatelessWidget {
+  const Medica({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
+  }
+}
