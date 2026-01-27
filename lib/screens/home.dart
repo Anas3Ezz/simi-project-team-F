476d6b5b0044994e4c588102b313e1cb37aa6bbb
Abdo-Elkholy <abdullah.ahmed.dev404@gmail.com>
2026-01-27 10:38:02 +0200

    search par widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/search_par.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: SafeArea(child: SearchPar()));
   }
 }
