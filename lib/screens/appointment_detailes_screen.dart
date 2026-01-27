a6bbbea5c52f14699bcd93a01ecef2ad6aaea265
anas <anas33ezz@gmail.com>
2026-01-27 17:13:14 +0200

    remove useless color

--- lib/screens/appointment_detailes_screen.dart
+++ lib/screens/appointment_detailes_screen.dart
@@ -6,8 +6,6 @@ class AppointmentDetailesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Defining the teal color used in the UI
-    const Color primaryTeal = Color(0xFF82C4C1);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
