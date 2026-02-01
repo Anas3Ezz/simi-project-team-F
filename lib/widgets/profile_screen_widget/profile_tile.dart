23b63071bc297173dc8439c87a308db6c5e4e49e
anas <anas33ezz@gmail.com>
2026-02-01 21:56:47 +0200

    create the logOut logic in profile screen

--- lib/widgets/profile_screen_widget/profile_tile.dart
+++ lib/widgets/profile_screen_widget/profile_tile.dart
@@ -6,32 +6,40 @@ class ProfileTile extends StatelessWidget {
   final IconData icon;
   final String title;
   final bool isLogout;
+  void Function()? onTap;
 
-  const ProfileTile({
+  ProfileTile({
     super.key,
     required this.icon,
     required this.title,
     this.isLogout = false,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 15),
-      child: ListTile(
-        leading: Container(
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: AppColors.successGreen,
-            borderRadius: BorderRadius.circular(10),
+    return InkWell(
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 15),
+        child: ListTile(
+          leading: Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: AppColors.successGreen,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Icon(icon, color: AppColors.primaryTeal),
           ),
-          child: Icon(icon, color: AppColors.primaryTeal),
+          title: Text(title, style: AppTextStyles.heading2),
+          trailing: isLogout
+              ? null
+              : const Icon(
+                  Icons.arrow_forward_ios,
+                  size: 16,
+                  color: Colors.grey,
+                ),
+          onTap: onTap,
         ),
-        title: Text(title, style: AppTextStyles.heading2),
-        trailing: isLogout
-            ? null
-            : const Icon(Icons.arrow_forward_ios, size: 16, color: Colors.grey),
-        onTap: () {},
       ),
     );
   }
